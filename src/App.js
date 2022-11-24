aff421d0d307e351e885938eb149a23fdf820c9b
Elroy He <elroyhe@Elroys-MacBook-Air.local>
2022-11-23 16:20:21 -0800

    basic setup done. I can add items to shopping cart and remove items from shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import prods from "./products";
+import "./App.css";
+const App = () => {
+  const [cart, setCart] = useState([]);
+  /**
+   * This is the onClickChange method used when user
+   * clicks on the products and adds them to the list
+   * @param event
+   */
+  const onChange = (event) => {
+    setCart((prevState) => [...prevState, event.target.textContent]);
+  };
 
-function App() {
+  /**
+   * This is the onDelete method used when user clicks
+   * on items in the cart and removes them from the cart
+   * @param event
+   */
+  const onDelete = (event) => {
+    console.log(event.target.id);
+    setCart((cart) =>
+      cart.filter((_, i) => i !== Number(event.target.id))
+    );
+  };
+
+  const cartList = cart.map((item, index) => {
+    return (
+      <div key={index} id={index} onClick={onDelete}>
+        {item}
+      </div>
+    );
+  });
+  const productList = prods.map((prod, index) => {
+    return index % 2 === 0 ? (
+      <div
+        key={index}
+        className="prod-item"
+        style={{ color: "red" }}
+        onClick={onChange}
+      >
+        {prod.name} {prod.price}
+      </div>
+    ) : (
+      <div key={index} className="prod-item" onClick={onChange}>
+        {prod.name} {prod.price}
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display">
+      <div className="left"> {productList}</div>
+      <div className="right"> {cartList}</div>
     </div>
   );
-}
+};
 
 export default App;
